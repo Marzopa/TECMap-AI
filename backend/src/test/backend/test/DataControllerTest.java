a2cc35ad1bbcc8bfe1254f72631ef4d9706ce432
oscar <ojimenezrodriguez@ithaca.edu>
2025-06-03 14:11:45 -0400

    tests don't pass, some higher level error with the dto

--- backend/src/test/backend/test/DataControllerTest.java
+++ backend/src/test/backend/test/DataControllerTest.java
@@ -1,5 +1,7 @@
 package backend.test;
 
+import API.SolveRequest;
+import API.SubmissionRequest;
 import Classroom.LearningMaterial;
 import Utils.Json;
 import org.junit.jupiter.api.Test;
@@ -17,14 +19,14 @@ public class DataControllerTest {
         String url = "http://localhost:8080/ai/problem";
 
         String jsonPayload = """
-        {
-            "topic": "hashmaps",
-            "difficulty": 5,
-            "additionalTopics": ["recursion", "loops", "conditionals", "arrays"],
-            "excludedTopics": ["caching", "concurrency", "recursion"],
-            "studentId": 705123456
-        }
-        """;
+                {
+                    "topic": "hashmaps",
+                    "difficulty": 5,
+                    "additionalTopics": ["recursion", "loops", "conditionals", "arrays"],
+                    "excludedTopics": ["caching", "concurrency"],
+                    "studentId": 705123456
+                }
+                """;
 
         System.out.println(">>>> SENDING TO OLLAMA: " + jsonPayload);
 
@@ -40,5 +42,18 @@ public class DataControllerTest {
         System.out.println("Body: " + postResponse.body());
         LearningMaterial glorbo = Json.fromJsonString(postResponse.body(), LearningMaterial.class);
         System.out.println("Glorbo item: " + glorbo.getAssessmentItem());
+
+        url = "http://localhost:8080/ai/submit";
+        SubmissionRequest request = new SubmissionRequest(glorbo, "System.out.println(\"Geeking\");", 705123456);
+        postRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(Json.toJsonString(request)))
+                .build();
+
+        postResponse = client.send(postRequest, HttpResponse.BodyHandlers.ofString());
+        System.out.println("POST /ai/problem response:");
+        System.out.println("Status: " + postResponse.statusCode());
+        System.out.println("Body: " + postResponse.body());
     }
 }
